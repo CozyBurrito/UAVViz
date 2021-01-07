338c7bfe27e6b862d6416edc3e3feac3d50ab7c1
mhameeducalgary <mohammad.hameed@ucalgary.ca>
2021-01-06 17:09:53 -0700

    Add base envioronment. Add AirSim.

---
+++ UAVSimEnv/Intermediate/Build/Win64/UAVSimEnvEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/M-WLP/School/GIT/UAVViz/UAVSimEnv/Intermediate/Build/Win64/UAVSimEnvEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.ShadowErrors.h"
---
+++ UAVSimEnv/Intermediate/Build/Win64/UAVSimEnvEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/M-WLP/School/GIT/UAVViz/UAVSimEnv/Intermediate/Build/Win64/UAVSimEnvEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
